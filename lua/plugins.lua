830060a38f2c6f7536efaa3eec5c85da27022097
Jacob Men <jacobmen416@yahoo.com>
2023-04-09 16:59:16 -0700

    add fuzzy search

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,7 +47,16 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				config = function()
+                    require('telescope').load_extension('fzf')
+                end,
+			},
+		},
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
