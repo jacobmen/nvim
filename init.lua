3a2321de67c79e6c621d7762d24808a1562c0c15
Jacob Men <jacobmen416@yahoo.com>
2023-04-09 20:55:50 -0700

    remove undodir

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.backup = false
-vim.opt.undodir = '~/.vim/undodir'
 vim.opt.undofile = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
